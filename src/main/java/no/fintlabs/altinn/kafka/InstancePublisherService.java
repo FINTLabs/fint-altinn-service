bf3ca96184a07e247c0e7feefcf807a0a96cbeb7
Ole Anders Eidjord <ole.anders.eidjord@vigoiks.no>
2024-11-13 14:04:36 +0100

    Logging

--- src/main/java/no/fintlabs/altinn/kafka/InstancePublisherService.java
+++ src/main/java/no/fintlabs/altinn/kafka/InstancePublisherService.java
@@ -8,6 +8,8 @@ import no.fintlabs.kafka.event.topic.EventTopicNameParameters;
 import no.fintlabs.kafka.event.topic.EventTopicService;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+
 @Slf4j
 @Service
 public class InstancePublisherService {
@@ -15,17 +17,19 @@ public class InstancePublisherService {
     private final EventProducer<Object> instanceExampleEventProducer;
     private final EventTopicNameParameters eventTopicNameParameters;
 
-
     public InstancePublisherService(EventProducerFactory eventProducerFactory, EventTopicService eventTopicService) {
         this.instanceExampleEventProducer = eventProducerFactory.createProducer(Object.class);
         this.eventTopicNameParameters = EventTopicNameParameters.builder()
-                .orgId("agderfk.no")
+                .orgId("fintlabs-no")
                 .eventName("altinn-instance")
+                .domainContext("drosje")
                 .build();
-        eventTopicService.ensureTopic(eventTopicNameParameters, 0);
+        log.info("Ensuring event topic: {}", eventTopicNameParameters);
+        eventTopicService.ensureTopic(eventTopicNameParameters, Duration.ofDays(1).toMillis());
     }
 
     public void publish(AltinnInstance altinnInstance) {
+        log.info("Publishing altinn instance: {}", altinnInstance);
         instanceExampleEventProducer.send(
                 EventProducerRecord.builder()
                         .topicNameParameters(eventTopicNameParameters)
