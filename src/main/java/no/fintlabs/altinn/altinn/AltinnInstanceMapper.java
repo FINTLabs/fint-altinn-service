8bfafe8267db9c46c46dab5f8876a48c5906f603
Ole Anders Eidjord <ole.anders.eidjord@vigoiks.no>
2024-12-02 15:56:49 +0100

    Mapping fields from application data

--- src/main/java/no/fintlabs/altinn/altinn/AltinnInstanceMapper.java
+++ src/main/java/no/fintlabs/altinn/altinn/AltinnInstanceMapper.java
@@ -1,18 +1,63 @@
 package no.fintlabs.altinn.altinn;
 
 import no.fint.altinn.model.kafka.KafkaAltinnInstance;
-import no.fintlabs.altinn.altinn.model.AltinnInstance;
-import no.fintlabs.altinn.altinn.model.ApplicationModel;
+import no.fintlabs.altinn.altinn.model.*;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 
 public class AltinnInstanceMapper {
 
-    public static KafkaAltinnInstance mapToAltinnInstance(AltinnInstance instanceFromAltinn, ApplicationModel t2) {
+    public static KafkaAltinnInstance mapToAltinnInstance(AltinnInstance instanceFromAltinn, ApplicationModel applicationModel) {
         return KafkaAltinnInstance.builder()
                 .instanceId(instanceFromAltinn.getId())
+
                 .organizationNumber(instanceFromAltinn.getInstanceOwner().getOrganisationNumber())
-                .countyCode(t2.getVirksomhet().getFylke().getFylkesnummer())
-                .countyName(t2.getVirksomhet().getFylke().getFylkesnavn())
+                .organizationName(applicationModel.getVirksomhet().getOrganisasjonsnavn())
+
+                .countyNumber(applicationModel.getVirksomhet().getFylke().getFylkesnummer())
+                .countyName(applicationModel.getVirksomhet().getFylke().getFylkesnavn())
+
+                .companyEmail(Optional.ofNullable(applicationModel.getVirksomhet())
+                        .map(ApplicationVirksomhet::getKontaktinformasjon)
+                        .map(ApplicationKontaktinformasjon::getEpostadresse).orElse(null))
+
+                .companyPhone(Optional.ofNullable(applicationModel.getVirksomhet())
+                        .map(ApplicationVirksomhet::getKontaktinformasjon)
+                        .map(ApplicationKontaktinformasjon::getTelefonnummer).orElse(null))
+
+                .companyAdressStreet(Optional.ofNullable(applicationModel.getVirksomhet())
+                        .map(ApplicationVirksomhet::getForretningsadresse)
+                        .map(ApplicationAdresse::getAdresse).orElse(null))
+                .companyAdressPostcode(Optional.ofNullable(applicationModel.getVirksomhet())
+                        .map(ApplicationVirksomhet::getForretningsadresse)
+                        .map(ApplicationAdresse::getPostnummer).orElse(null))
+                .companyAdressPostplace(Optional.ofNullable(applicationModel.getVirksomhet())
+                        .map(ApplicationVirksomhet::getForretningsadresse)
+                        .map(ApplicationAdresse::getPoststed).orElse(null))
+
+                .postalAdressStreet(Optional.ofNullable(applicationModel.getVirksomhet())
+                        .map(ApplicationVirksomhet::getPostadresse)
+                        .map(ApplicationAdresse::getAdresse).orElse(null))
+                .postalAdressPostcode(Optional.ofNullable(applicationModel.getVirksomhet())
+                        .map(ApplicationVirksomhet::getPostadresse)
+                        .map(ApplicationAdresse::getPostnummer).orElse(null))
+                .postalAdressPostplace(Optional.ofNullable(applicationModel.getVirksomhet())
+                        .map(ApplicationVirksomhet::getPostadresse)
+                        .map(ApplicationAdresse::getPoststed).orElse(null))
+
+                .managerSocialSecurityNumber(applicationModel.getDagligLeder().getFodselsnummer())
+                .managerFirstName(applicationModel.getDagligLeder().getFornavn())
+                .managerLastName(applicationModel.getDagligLeder().getEtternavn())
+                
+                .managerEmail(Optional.ofNullable(applicationModel.getDagligLeder())
+                        .map(ApplicationDagligLeder::getKontaktinformasjon)
+                        .map(ApplicationKontaktinformasjon::getEpostadresse).orElse(null))
+                .managerPhone(Optional.ofNullable(applicationModel.getDagligLeder())
+                        .map(ApplicationDagligLeder::getKontaktinformasjon)
+                        .map(ApplicationKontaktinformasjon::getTelefonnummer).orElse(null))
+
                 .build();
     }
 }
