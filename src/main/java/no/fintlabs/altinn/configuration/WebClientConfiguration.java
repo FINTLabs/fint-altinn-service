f9d079d214ade9a9ca8b1113c6bbc7d3a8083349
Ole Anders Eidjord <26850+oleanders@users.noreply.github.com>
2025-06-20 11:20:00 +0200

    File size limit

--- src/main/java/no/fintlabs/altinn/configuration/WebClientConfiguration.java
+++ src/main/java/no/fintlabs/altinn/configuration/WebClientConfiguration.java
@@ -2,13 +2,23 @@ package no.fintlabs.altinn.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfiguration {
 
+    private static final int MAX_IN_MEMORY_SIZE = 50 * 1024 * 1024;
+
     @Bean
     public WebClient webClient() {
-        return WebClient.builder().build();
+        ExchangeStrategies strategies = ExchangeStrategies.builder()
+                .codecs(clientCodecConfigurer ->
+                    clientCodecConfigurer.defaultCodecs().maxInMemorySize(MAX_IN_MEMORY_SIZE))
+                .build();
+
+        return WebClient.builder()
+                .exchangeStrategies(strategies)
+                .build();
     }
 }
