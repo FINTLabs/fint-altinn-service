ae4f8afae0b417a9152a2af553cd5bf1344e71e8
Nils-Odd Solberg <nilsodd@fintlabs.no>
2026-01-12 15:03:40 +0100

    Add @EqualsAndHashCode to avoid warning

--- src/main/java/no/novari/altinn/altinn/model/DrosjesentralApplicationModel.java
+++ src/main/java/no/novari/altinn/altinn/model/DrosjesentralApplicationModel.java
@@ -5,10 +5,12 @@ import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlRootElement;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @XmlRootElement(name = "Datamodell")
 @XmlAccessorType(XmlAccessType.FIELD)
 @Data
+@EqualsAndHashCode(callSuper=false)
 public class DrosjesentralApplicationModel extends AltinnApplicationModel {
     
     @XmlElement
